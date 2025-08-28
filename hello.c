e60c2046078802c12affd0ac729761861046288d
DesireTN21 <dtonyenyemeck2@huskers.unl.edu>
2025-08-28 10:35:15 -0500

    intitial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * A simple hello world program in C
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'M Desire Tonye Nyemeck!\n");
+printf("Major Electrical Engineering!\n");
+
+  return 0;
+}
